2b495d1d856a0ae278393917630e8330b48eb5d2
bens <5878794@qq.com>
2023-04-15 21:26:31 +0800

    start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,48 +3,48 @@ import boxStyle from '@/css/box.module.scss'
 import pageStyle from '@/css/page.module.scss'
 import {shallowRef} from "vue";
 
-const files = require.context('./',true,/.vue$/);
-const components:any = [];
-files.keys().forEach((path)=>{
+const files = require.context('./', true, /.vue$/);
+const components: any = [];
+files.keys().forEach((path) => {
     const key = path.split('/')[1];
-    components.push({key:key,com:files(path).default})
+    components.push({key: key, com: files(path).default})
 })
 
-class Index{
-    props:any;
-    opts:any;
-    show:any = shallowRef('');
+class Index {
+    props: any;
+    opts: any;
+    show: any = shallowRef('');
 
-    constructor(props:any,opts:any) {
+    constructor(props: any, opts: any) {
         this.props = props;
         this.opts = opts;
 
         this.show.value = components[0].com;
     }
 
-    renderCom(){
+    renderCom() {
         const tag = this.show.value;
         return <tag/>
     }
 
-    change(i:number){
+    change(i: number) {
         this.show.value = components[i].com;
     }
 
-    ready(){
+    ready() {
         console.log(123)
     }
 
-    destroy(){
+    destroy() {
         console.log('end')
     }
 
-    render(){
+    render() {
         return <div class={[boxStyle.box_hlt]}>
             <div class={[pageStyle.left]} style='cursor:pointer;line-height:20px;'>
-                {components.map((rs:any,i:number)=>{
+                {components.map((rs: any, i: number) => {
                     return <div
-                        onClick={()=> this.change(i)}
+                        onClick={() => this.change(i)}
                     >{rs.key}</div>
                 })}
             </div>
